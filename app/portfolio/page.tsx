9a0fee81e1b4b1bb2125b124406ce1b3dcbbbea0
nzamaesarah-hub <nzamaesarah@gmail.com>
2026-07-22 20:56:50 +0800

    First commit - Project Next.js

---
+++ app/portfolio/page.tsx
@@ -0,0 +1,8 @@
+export default function PortfolioPage() {
+  return (
+    <div className="bg-white p-6 rounded-lg shadow-sm">
+      <h1 className="text-3xl font-bold mb-4">Halaman Portfolio</h1>
+      <p className="text-gray-600">Berikut adalah daftar proyek atau karya yang pernah saya buat.</p>
+    </div>
+  );
+}
---
+++ app/profile/page.tsx
@@ -0,0 +1,8 @@
+export default function ProfilePage() {
+  return (
+    <div className="bg-white p-6 rounded-lg shadow-sm">
+      <h1 className="text-3xl font-bold mb-4">Halaman Profile</h1>
+      <p className="text-gray-600">Halo! Ini adalah halaman yang berisi informasi tentang profil saya.</p>
+    </div>
+  );
+}
